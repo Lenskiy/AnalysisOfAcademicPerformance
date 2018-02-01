96c245d9c007ae1029c29b3a16c59653eaa6ea29
Lenskiy <a.a.lensky@gmail.com>
2018-02-01 19:56:03 +1100

    Added a new function for calculating histogram of number of classes a student has take in each class type.

--- script2.m
+++ script2.m
@@ -38,3 +38,47 @@ visualizeGPAHistogramBeforeAndAfter(gpaNoBreak, (gpaBeforeBreak + gpaAfterBreak)
 % plot(gpaAfterBreak)
 visualizeGPAHistogramBeforeAndAfter(gpaBeforeBreak, gpaAfterBreak, 'Electronics: GPA before and after', 'before', 'after');
 visualizeGPAHistogramBeforeAndAfter(gpaNoBreak, (gpaBeforeBreak + gpaAfterBreak) / 2,'Electronics: Effect of a break', 'no break', 'break');
+
+%build a histogram of classes categories
+cathist = buildClassCategoryHistogram(allRecords);
+
+% remove categories that appear rarely
+catInds = find(sum(cathist) >= 10);
+
+rcathist = cathist(:, catInds);
+% selected types labels
+for k = 1:length(catInds)
+    stypes{k} = types{catInds(k)};
+end
+
+% claculte overall GPA for each student 
+gpaOveral = calcOverAllGPA(allRecords);
+% Normalize GPA to [0, 1] to be able to use it as a color
+gpaOveralNorm = (gpaOveral - min(gpaOveral)) / (max(gpaOveral) - min(gpaOveral));
+
+
+figure, hold on, grid on;
+for k = 1:size(allRecords,2)
+   plot3(k * ones(1, size(rcathist(k, :),2)), 1:size(rcathist(k, :),2), rcathist(k, :), 'color', [gpaOveralNorm(k), 1 - gpaOveralNorm(k), 0], 'marker', '.')
+end
+xlabel('students');
+ylabel('categories');
+zlabel('times');
+axis([1, size(allRecords,2), 1, length(stypes), 0, max(max(rcathist))]);
+ax = gca;
+ax.YTick = [1:length(stypes)];
+ax.YTickLabel = stypes;
+set(gca, 'YTickLabelRotation', 45)
+    
+% Find principal components
+[pc, score, latent, tsquare] = pca(rcathist);
+
+reduced = rcathist * pc(:,1:3);
+
+figure, hold on, grid on;
+for k = 1:size(allRecords,2)
+    plot3(reduced(k,1), reduced(k,2), reduced(k,3), 'color', [gpaOveralNorm(k), 1 - gpaOveralNorm(k), 0], 'marker', '.');
+end
+xlabel('component 1');
+ylabel('component 2');
+zlabel('component 3');
